ebc9e4607791e31fae7560d5265e2bd06ef65e09
irgbit <birgit.bachler@gmail.com>
2019-02-04 16:10:59 +1300

    not sure tee hee

--- Papawai/Papawai_Transmissions/Papawai-Transmissions.turbidity/Papawai-Transmissions.turbidity.ino
+++ Papawai/Papawai_Transmissions/Papawai-Transmissions.turbidity/Papawai-Transmissions.turbidity.ino
@@ -1,13 +1,13 @@
-//             _ _     _ _   _     _                   _       
-//            (_) |   | | | | |   (_)                 (_)      
-//   __      ___| | __| | |_| |__  _ _ __   __ _ ___   _  ___  
+//             _ _     _ _   _     _                   _
+//            (_) |   | | | | |   (_)                 (_)
+//   __      ___| | __| | |_| |__  _ _ __   __ _ ___   _  ___
 //   \ \ /\ / / | |/ _` | __| '_ \| | '_ \ / _` / __| | |/ _ \ 
 //    \ V  V /| | | (_| | |_| | | | | | | | (_| \__ \_| | (_) |
-//     \_/\_/ |_|_|\__,_|\__|_| |_|_|_| |_|\__, |___(_)_|\___/ 
-                                        __/ |              
-                                       |___/               
+//     \_/\_/ |_|_|\__,_|\__|_| |_|_|_| |_|\__, |___(_)_|\___/
+//                                         __ / |
+//                                       | ___ /
 
-//   Papawai Transmissions 
+//   Papawai Transmissions
 //   1st Prototype for Wemos D1 mini for measuring turbidity
 //
 //   wildthings.io - Birgit Bachler, Aotearoa/New Zealand, 2019
@@ -18,7 +18,7 @@
 int sensorPin = A0;    // select the input pin for the LDR
 int sensorValue = 0;  // variable to store the value coming from the sensor
 int ledPin = D1; // select output pin for LED
-int ledValue = 64; // low light LED 
+int ledValue = 512; // low light LED
 
 boolean debug = true;
 void setup() {
@@ -27,12 +27,13 @@ void setup() {
 }
 
 void loop() {
-  // read the value from the sensor:
-  analogWrite(ledPin, ledValue);
-  delay(10);
-  sensorValue = analogRead(sensorPin);
-  Serial.println(sensorValue);
+  //for (int x = 0; x < 1024; x += 100) {
+    analogWrite(ledPin, ledValue);
+    delay(10);
+    sensorValue = analogRead(sensorPin);  // read the value from the sensor:
+    Serial.println(sensorValue);
 
-  delay(500);
+    delay(500);
+ // }
 
 }
